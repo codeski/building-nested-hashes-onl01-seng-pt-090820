40d297c065f35d7e4d0ee2bdd3101036c3eeee00
Todd Stoneberg <todd.stoneberg@gmail.com>
2020-09-17 22:10:52 +0000

    Automatically backed up by Learn

--- lib/third_challenge.rb
+++ lib/third_challenge.rb
@@ -27,13 +27,14 @@ def third_challenge
         },
       :matriarch => {
         #your key/value pairs here
-        :name =>
-        :age =>
+        :name => "Lady Capulet"
+        :age => "51"
         },
       :heroine => {
         #your key/value pairs here
-        :name =>
-        :age =>
+        :name => "Juliet"
+        :age => "15"
+        :status => "alive"
         },
       :heroine_friends => []
    }

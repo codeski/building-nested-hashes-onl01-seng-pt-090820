ec3fb1e10017c056e0791db0a44f0418b5e809cc
Todd Stoneberg <todd.stoneberg@gmail.com>
2020-09-17 21:58:14 +0000

    Automatically backed up by Learn

--- lib/third_challenge.rb
+++ lib/third_challenge.rb
@@ -8,6 +8,8 @@ def third_challenge
         },
       :matriarch => {
         #your key/value pairs here
+        :name =>
+        :age =>
         },
       :hero => {
         #your key/value pairs here
